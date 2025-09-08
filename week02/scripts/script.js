2843eea7c62da694087c13ecda08cdb28b433bbf
Igor2games <igor1games@outlook.com>
2025-09-08 18:54:18 -0300

    BOM finished

--- week02/scripts/script.js
+++ week02/scripts/script.js
@@ -1,15 +1,30 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('______');
+const list = document.querySelector('#list');
 
-const li = document.createElement('li');
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+    
+        const li = document.createElement('li');
+        
+        const deleteButton = document.createElement('button');
+        
+        li.textContent = input.value;
 
-const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
 
-li.textContent = input.value;
+        li.append(deleteButton);
 
-deleteButton.textContent = '❌';
+        list.append(li);
 
-li.append(deleteButton);
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
 
-list.append(li);
+        input.value = '';
+
+        input.focus();
+
+    }
+});
